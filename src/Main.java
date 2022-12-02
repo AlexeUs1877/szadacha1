27db4fa7b0f10b34cfbade34169dfe48d3f80242
Alexey <alexeyz1877@gmail.com>
2022-12-02 12:55:33 +0300

    Задача сделана

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+int number = 15;
+float a = 8.5f;                        // два числа a и b для определения из них ближайшего к number
+float b = 12.8f;
+boolean nearest = (a - number) > (b - number);
+if (a >= number || b >= number) {
+    System.out.println("Число не должно быть больше или равно проверяемому.");
+}
+else if (a == b) {
+    System.out.println("Введите разные числа.");
+}
+else if (nearest) {
+    System.out.println("Ближайшее число к проверяемому равно: " + a);
+}
+else {
+    System.out.println("Ближайшее число к проверяемому равно: " + b);
+}
 
     }
 }
